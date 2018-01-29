9b5b904dc15d75e4f7b662c3db7771efc5ff318c
Ильнар Мухутдинов <prizrak-im@inbox.ru>
2018-01-29 17:58:24 +0300

    Добавил возможность внесения данных по умолчанию для отладки

---
+++ DataBase Course Work/DefaultInfoInDb.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataBase_Course_Work
+{
+    class DefaultInfoInDb
+    {
+        public Context DataContext;
+
+        private CourtCase _courtCase;
+
+        private Employee _employee;
+
+        private Protocol _protocol;
+
+        private CaseMaterial _caseMaterial;
+
+        private Plaintiff _plaintiff;
+
+        private Defendant _defendant;
+
+        private readonly string[] _firstnames = { "Донна", "Хелен", "Элис", "Энн", "Кларисса", "Линда", "Мэрилинн" };
+
+        private readonly string[] _secondnames = { "Хортон", "Маккой", "Уолтон", "Бэйкер", "Митчелл", "Итон", "Паркс" };
+
+        private readonly string[] _lastnames = { "Джулия", "Кэролайн", "Дженнифер", "Дороти", "Эмбер", "Беверли", "Джейд" };
+
+        private readonly string[] _streets = {"694 W. Charles Street ", "93 Franklin St. ", "9845 S. Devonshire Drive", "594 Charles Ave. ",
+            "915 NW. Cooper St.", "7701 Acacia Lane", "7650 West Hawthorne Avenue", "57 Manor Dr.", "675 NE. Mill Pond Circle", "78 S. St Louis Street" };
+
+        public Context CreateDefaultEmployee()
+        {
+            _employee = new Employee
+            {
+                FirstName = "Джайлс",
+                SecondName = "Хайнс",
+                LastName = "Питер",
+                BirthDay = new DateTime(1977, 3, 6),
+                Location = "Wayne, NJ 07470",
+                Post = "Судья"
+            };
+            DataContext.Employees.Add(_employee);
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < 4; i++)
+            {
+                _employee = new Employee
+                {
+                    FirstName = _firstnames[rnd.Next(0, _firstnames.Length)],
+                    SecondName = _secondnames[rnd.Next(0, _secondnames.Length)],
+                    LastName = _lastnames[rnd.Next(0, _lastnames.Length)],
+                    BirthDay = new DateTime(rnd.Next(1970, 1997), rnd.Next(1, 11), rnd.Next(1, 25)),
+                    Location = _streets[rnd.Next(0, _streets.Length)],
+                    Post = "Секретарь"
+                };
+                DataContext.Employees.Add(_employee);
+            }
+            return DataContext;
+        }
+
+        public Context CreateDefaultDefendant()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                _defendant = new Defendant
+                {
+                    FirstName = _firstnames[rnd.Next(0, _firstnames.Length)],
+                    SecondName = _secondnames[rnd.Next(0, _secondnames.Length)],
+                    LastName = _lastnames[rnd.Next(0, _lastnames.Length)],
+                    BirthDay = new DateTime(rnd.Next(1970, 1997), rnd.Next(1, 11), rnd.Next(1, 25)),
+                    PasportNum = rnd.Next(44, 55),
+                    PasportSeries = rnd.Next(44444, 99999)
+                };
+                DataContext.Defendants.Add(_defendant);
+            }
+
+            return DataContext;
+        }
+
+        public Context CreateDefaultPlaintiff()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Random rnd = new Random();
+                _plaintiff = new Plaintiff
+                {
+                    FirstName = _firstnames[rnd.Next(0, _firstnames.Length)],
+                    SecondName = _secondnames[rnd.Next(0, _secondnames.Length)],
+                    LastName = _lastnames[rnd.Next(0, _lastnames.Length)],
+                    BirthDay = new DateTime(rnd.Next(1970, 1997), rnd.Next(1, 11), rnd.Next(1, 25)),
+                    PasportNum = rnd.Next(44, 55),
+                    PasportSeries = rnd.Next(44444, 99999)
+                };
+                DataContext.Plaintiffs.Add(_plaintiff);
+            }
+            return DataContext;
+        }
+
+        public Context CreateDefaultProtocol()
+        {
+            _protocol = new Protocol
+            {
+                WitnessReadings = "Преступника не видели",
+                DefendantReadings = "Я не виновна",
+                PlaintiffReadings = "Она виновна"
+            };
+
+            DataContext.Protocols.Add(_protocol);
+            return DataContext;
+        }
+
+        public Context CreateDefaultCaseMaterial()
+        {
+            _caseMaterial = new CaseMaterial()
+            {
+                Evidence = "Убийство на Таймс-сквер"
+            };
+            DataContext = new Context();
+            DataContext.CaseMaterials.Add(_caseMaterial);
+            return DataContext;
+        }
+
+        public Context CreateDefaultCourtCase()
+        {
+            _courtCase = new CourtCase
+            {
+                StartDateTime = new DateTime(2000, 5, 20),
+                EndDateTime = new DateTime(2000, 8, 14),
+                EmployeeId = 1,
+                Decision = "Оправдать"
+            };            
+            DataContext = new Context();
+            DataContext.CourtCases.Add(_courtCase);
+            return DataContext;
+        }
+    }
+}
--- DataBase Course Work/MainWindow.xaml.cs
+++ DataBase Course Work/MainWindow.xaml.cs
@@ -11,168 +11,94 @@ namespace DataBase_Course_Work
 {
     public partial class MainWindow : Window
     {
-        private readonly Context dataContext;
-        private CourtCase courtCase;
-        private Employee employee;
-        private Protocol protocol;
-        private CaseMaterial caseMaterial;
-        private Plaintiff plaintiff;
-        private Defendant defendant;
-        private FindCaseParams fcParams;
+        private Context _dataContext;
 
         public MainWindow()
         {
             InitializeComponent();
-            dataContext = new Context();
+            _dataContext = new Context();
+            DefaultInfoInDb defaultInfo = new DefaultInfoInDb();
+            //_dataContext = defaultInfo.CreateDefaultCaseMaterial();
+            //_dataContext = defaultInfo.CreateDefaultCourtCase();
             Database.SetInitializer(new DropCreateDatabaseIfModelChanges<Context>());
-            //CreateDefaultInfoInDB();
         }
 
         private void Window_Closing(object sender, CancelEventArgs e)
         {
-            dataContext.Dispose();
+            _dataContext.Dispose();
         }
 
         private void ItemCourtCase_Selected(object sender, RoutedEventArgs e)
         {
-            dataContext.CourtCases.Load();
-            MainGrid.ItemsSource = dataContext.CourtCases.Local.ToBindingList();
+            _dataContext.CourtCases.Load();
+            MainGrid.ItemsSource = _dataContext.CourtCases.Local.ToBindingList();
         }
 
         private void ItemEmployee_Selected(object sender, RoutedEventArgs e)
         {
-            dataContext.Employees.Load();
-            MainGrid.ItemsSource = dataContext.Employees.Local.ToBindingList();
+            _dataContext.Employees.Load();
+            MainGrid.ItemsSource = _dataContext.Employees.Local.ToBindingList();
         }
 
-        private void Protocol_Selected(object sender, RoutedEventArgs e)
+        private void ItemProtocol_Selected(object sender, RoutedEventArgs e)
         {
-            dataContext.Protocols.Load();
-            MainGrid.ItemsSource = dataContext.Protocols.Local.ToBindingList();
+            _dataContext.Protocols.Load();
+            MainGrid.ItemsSource = _dataContext.Protocols.Local.ToBindingList();
         }
 
-        private void CaseMaterials_Selected(object sender, RoutedEventArgs e)
+        private void ItemCaseMaterials_Selected(object sender, RoutedEventArgs e)
         {
-            dataContext.CaseMaterials.Load();
-            MainGrid.ItemsSource = dataContext.CaseMaterials.Local.ToBindingList();
+            _dataContext.CaseMaterials.Load();
+            MainGrid.ItemsSource = _dataContext.CaseMaterials.Local.ToBindingList();
         }
 
-        private void Plaintiff_Selected(object sender, RoutedEventArgs e)
+        private void ItemPlaintiff_Selected(object sender, RoutedEventArgs e)
         {
-            dataContext.Plaintiffs.Load();
-            MainGrid.ItemsSource = dataContext.Plaintiffs.Local.ToBindingList();
+            _dataContext.Plaintiffs.Load();
+            MainGrid.ItemsSource = _dataContext.Plaintiffs.Local.ToBindingList();
         }
 
-        private void Defendant_Selected(object sender, RoutedEventArgs e)
+        private void ItemDefendant_Selected(object sender, RoutedEventArgs e)
         {
-            dataContext.Defendants.Load();
-            MainGrid.ItemsSource = dataContext.Defendants.Local.ToBindingList();
+            _dataContext.Defendants.Load();
+            MainGrid.ItemsSource = _dataContext.Defendants.Local.ToBindingList();
         }
 
-        private void btn_UpdateDB_Click(object sender, RoutedEventArgs e)
+        private void BtnUpdateDB_Click(object sender, RoutedEventArgs e)
         {
-            dataContext.SaveChanges();
+            _dataContext.SaveChanges();
         }
 
         private void ItemCourtCaseDelete_Selected(object sender, RoutedEventArgs e)
         {
-            dataContext.Database.ExecuteSqlCommand("DELETE FROM [CourtCases]");
+            _dataContext.Database.ExecuteSqlCommand("DELETE FROM [CourtCases]");
         }
 
         private void ItemEmployeeDelete_Selected(object sender, RoutedEventArgs e)
         {
-            dataContext.Database.ExecuteSqlCommand("DELETE FROM [Employees]");
+            _dataContext.Database.ExecuteSqlCommand("DELETE FROM [Employees]");
         }
 
         private void ItemProtocolDelete_Selected(object sender, RoutedEventArgs e)
         {
-            dataContext.Database.ExecuteSqlCommand("DELETE FROM [Protocols]");
+            _dataContext.Database.ExecuteSqlCommand("DELETE FROM [Protocols]");
         }
 
         private void ItemCaseMaterialsDelete_Selected(object sender, RoutedEventArgs e)
         {
-            dataContext.Database.ExecuteSqlCommand("DELETE FROM [CaseMaterials]");
+            _dataContext.Database.ExecuteSqlCommand("DELETE FROM [CaseMaterials]");
         }
 
         private void ItemPlaintiffDelete_Selected(object sender, RoutedEventArgs e)
         {
-            dataContext.Database.ExecuteSqlCommand("DELETE FROM [Plaintiffs]");
+            _dataContext.Database.ExecuteSqlCommand("DELETE FROM [Plaintiffs]");
         }
 
         private void ItemDefendantDelete_Selected(object sender, RoutedEventArgs e)
         {
-            dataContext.Database.ExecuteSqlCommand("DELETE FROM [Defendants]");
+            _dataContext.Database.ExecuteSqlCommand("DELETE FROM [Defendants]");
         }
-
-        private void CreateDefaultInfoInDB()
-        {
-            courtCase = new CourtCase();
-            string[] firstnames = {"Донна", "Хелен", "Элис", "Энн", "Кларисса", "Линда", "Мэрилинн"};
-            string[] secondnames = {"Хортон", "Маккой", "Уолтон", "Бэйкер", "Митчелл", "Итон", "Паркс"};
-            string[] lastnames = {"Джулия", "Кэролайн", "Дженнифер", "Дороти", "Эмбер", "Беверли", "Джейд"};
-            string[] streets = {
-                "694 W. Charles Street ", "93 Franklin St. ",
-                "9845 S. Devonshire Drive", "594 Charles Ave. ",
-                "915 NW. Cooper St.", "7701 Acacia Lane",
-                "7650 West Hawthorne Avenue", "57 Manor Dr.",
-                "675 NE. Mill Pond Circle", "78 S. St Louis Street"
-            };
-            employee = new Employee
-            {
-                FirstName = "Джайлс",
-                SecondName = "Хайнс",
-                LastName = "Питер",
-                BirthDay = new DateTime(1977, 3, 6),
-                Location = "Wayne, NJ 07470",
-                Post = "Судья"
-            };
-            dataContext.Employees.Add(employee);
-            
-            Random rnd = new Random();
-
-            for (int i = 0; i < 4; i++)
-            {
-                employee = new Employee
-                {
-                    FirstName = firstnames[rnd.Next(0, firstnames.Length)],
-                    SecondName = secondnames[rnd.Next(0, secondnames.Length)],
-                    LastName = lastnames[rnd.Next(0, lastnames.Length)],
-                    BirthDay = new DateTime(rnd.Next(1970, 1997), rnd.Next(1, 11), rnd.Next(1, 25)),
-                    Location = streets[rnd.Next(0, streets.Length)],
-                    Post = "Секретарь"
-                };
-                dataContext.Employees.Add(employee);
-            }
-
-            for (int i = 0; i < 5; i++)
-            {
-                plaintiff = new Plaintiff
-                {
-                    FirstName = firstnames[rnd.Next(0, firstnames.Length)],
-                    SecondName = secondnames[rnd.Next(0, secondnames.Length)],
-                    LastName = lastnames[rnd.Next(0, lastnames.Length)],
-                    BirthDay = new DateTime(rnd.Next(1970, 1997), rnd.Next(1, 11), rnd.Next(1, 25)),
-                    PasportNum = rnd.Next(44, 55),
-                    PasportSeries = rnd.Next(44444, 99999)
-                };
-                dataContext.Plaintiffs.Add(plaintiff);
-            }
-
-            for (int i = 0; i < 5; i++)
-            {
-                defendant = new Defendant
-                {
-                    FirstName = firstnames[rnd.Next(0, firstnames.Length)],
-                    SecondName = secondnames[rnd.Next(0, secondnames.Length)],
-                    LastName = lastnames[rnd.Next(0, lastnames.Length)],
-                    BirthDay = new DateTime(rnd.Next(1970, 1997), rnd.Next(1, 11), rnd.Next(1, 25)),
-                    PasportNum = rnd.Next(44, 55),
-                    PasportSeries = rnd.Next(44444, 99999)
-                };
-                dataContext.Defendants.Add(defendant);
-            }
-        }
-
+        
         private void ItemFindCaseByDate_Selected(object sender, RoutedEventArgs e)
         {
             FindCaseParams fcp = new FindCaseParams() {Owner = this};
@@ -250,6 +176,18 @@ namespace DataBase_Course_Work
                 }
             }
             return null;
+        }
+
+        private void BtnFillDefaultInfo_Click(object sender, RoutedEventArgs e)
+        {
+            DefaultInfoInDb defaultInfo = new DefaultInfoInDb();
+            _dataContext = defaultInfo.CreateDefaultEmployee();
+            _dataContext = defaultInfo.CreateDefaultDefendant();
+            _dataContext = defaultInfo.CreateDefaultPlaintiff();
+            _dataContext = defaultInfo.CreateDefaultCaseMaterial();
+            _dataContext = defaultInfo.CreateDefaultProtocol();
+            _dataContext = defaultInfo.CreateDefaultCourtCase();
+            _dataContext.SaveChanges();
         }
     }
 }
